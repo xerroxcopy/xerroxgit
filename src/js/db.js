8aa73b7a6390442d0188b43c2ed196ffdbf1c17d
xerroxcopy <xerroxcopy@gmail.com>
2020-08-03 20:14:58 +0900

    nicer colors

--- src/js/db.js
+++ src/js/db.js
@@ -1,16 +1,18 @@
 const store = new SteinStore(
   'https://api.steinhq.com/v1/storages/5efd782883c30d0425e2c5ba'
 )
-let publications = []
-store.read('publication', { limit: 100, offset: 0 }).then((data) => {
+export let publications = []
+store.read('publication', {limit: 100, offset: 0}).then((data) => {
   console.dir(data)
   publications = data
   const publicationTypesArr = publications.map(
     (publication) => publication.type
   )
+  console.log(publicationTypesArr)
   const publicationTypes = publicationTypesArr.filter((elem, index, self) => {
     self.indexOf(elem) === index
   })
+  console.log('publicationTypes', publicationTypes)
 })
 
-module.exports = { publications: publications }
+// module.exports = {publications: publications}
